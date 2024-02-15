3f80cba564c13b781bebe0cc043001493403a0a8
stan <stan>
2024-02-15 13:48:22 -0300

    task4

---
+++ Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿int a = 6;
+int b = 7;
+int c = 10;
+int d = 9;
+int e = 8;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
+
+
