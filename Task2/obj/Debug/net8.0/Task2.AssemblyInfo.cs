bcac33f3a30506dd7a30993f1773d9bbabe07267
stan <stan>
2024-02-15 13:24:22 -0300

    operations

--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d97783f36a454c3e85aa7542fd35f1ec02f01b5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ef804dd32977b2c29c8bce404decb8dd0a6c467")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
